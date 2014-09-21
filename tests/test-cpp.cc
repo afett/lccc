0e438c1c32ad2fff79140869a31d6881a9d52df8
Andreas Fett <a.fett@gmx.de>
2014-09-21 11:21:09 +0200

    tests: add tests for cpp_* classes

--- tests/test-cpp.cc
+++ tests/test-cpp.cc
@@ -1,6 +1,10 @@
+#include <sstream>
 #include <cppunit/extensions/HelperMacros.h>
+#include <lccc/cpp.h>
+#include <lccc/header.h>
 
 namespace unittests {
+namespace cpp {
 
 class test : public CppUnit::TestCase {
 public:
@@ -9,10 +13,20 @@ public:
 	void tearDown();
 
 private:
-	void basic();
+	void test_define();
+	void test_include();
+	void test_ifdef();
+	void test_ifndef();
+	void test_guard();
+	void test_header();
 
 	CPPUNIT_TEST_SUITE(test);
-	CPPUNIT_TEST(basic);
+	CPPUNIT_TEST(test_define);
+	CPPUNIT_TEST(test_include);
+	CPPUNIT_TEST(test_ifdef);
+	CPPUNIT_TEST(test_ifndef);
+	CPPUNIT_TEST(test_guard);
+	CPPUNIT_TEST(test_header);
 	CPPUNIT_TEST_SUITE_END();
 };
 
@@ -27,9 +41,81 @@ void test::setUp()
 void test::tearDown()
 { }
 
-void test::basic()
+void test::test_define()
 {
+	lccc::cpp_define::ptr_t def(lccc::cpp_define::make("FOO"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected("#define FOO\n");
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
 }
 
+void test::test_include()
+{
+	lccc::cpp_include::ptr_t def(lccc::cpp_include::make("foo.h"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected("#include<foo.h>\n");
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
 }
+
+void test::test_ifdef()
+{
+	lccc::cpp_ifdef::ptr_t def(lccc::cpp_ifdef::make("FOOBAR"));
+	def->add(lccc::cpp_define::make("FOO"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected(
+		"#ifdef FOOBAR\n"
+		"#define FOO\n"
+		"#endif\n"
+	);
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
+}
+
+void test::test_ifndef()
+{
+	lccc::cpp_ifndef::ptr_t def(lccc::cpp_ifndef::make("FOOBAR"));
+	def->add(lccc::cpp_define::make("FOO"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected(
+		"#ifndef FOOBAR\n"
+		"#define FOO\n"
+		"#endif\n"
+	);
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
+}
+
+void test::test_guard()
+{
+	lccc::cpp_guard::ptr_t def(lccc::cpp_guard::make("FOO_H"));
+	def->add(lccc::cpp_define::make("FOO"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected(
+		"#ifndef FOO_H\n"
+		"#define FOO_H\n"
+		"#define FOO\n"
+		"#endif\n"
+	);
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
+}
+
+void test::test_header()
+{
+	lccc::header::ptr_t def(lccc::header::make("lccc/cc_src.h"));
+	def->add(lccc::cpp_define::make("FOO"));
+	std::stringstream out;
+	def->print(out);
+	std::string expected(
+		"#ifndef LCCC_CC_SRC_H\n"
+		"#define LCCC_CC_SRC_H\n"
+		"#define FOO\n"
+		"#endif\n"
+	);
+	CPPUNIT_ASSERT_EQUAL(expected, out.str());
+}
+
+}}
 
